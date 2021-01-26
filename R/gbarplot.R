f4736d7663d2c33c6ea406f2245b8797f3c20a53
Tobie Surette <tobie.surette@gmail.com>
2021-01-26 14:35:58 -0400

    Update gbarplot.R

--- R/gbarplot.R
+++ R/gbarplot.R
@@ -80,9 +80,9 @@ gbarplot <- function(y, x, labels, width = 1, col = "grey", border = "grey50", a
    if (nrow(y) == 1) y <- t(y)
 
    # Define bar colours:
-   if (length(col) == 1)       col <- colorRampPalette(c("white", col))(ncol(y))
-   if (length(col) == ncol(y)) col <- gulf.utils::repvec(col, nrow = nrow(y))
-   if (length(border) == 1)       border <- colorRampPalette(c("white", border))(ncol(y))
+   if (length(col) == 1)          col <- colorRampPalette(c("grey80", col))(ncol(y))
+   if (length(col) == ncol(y))    col <- gulf.utils::repvec(col, nrow = nrow(y))
+   if (length(border) == 1)       border <- colorRampPalette(c("grey70", border))(ncol(y))
    if (length(border) == ncol(y)) border <- gulf.utils::repvec(border, nrow = nrow(y))
    
    # Define 'x' as an integer sequence if undefined:
